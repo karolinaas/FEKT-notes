b9c69493c9e92ab6430cbf477a6d6f74509f39bf
Šebestová <ka.sebestova.cz@gmail.com>
2025-03-18 23:10:48 +0100

    Update snippets.js

--- docs/_static/js/snippets.js
+++ docs/_static/js/snippets.js
@@ -6,17 +6,3 @@ admonition.forEach((item) => {
 		item.style.fontFamily = "var(--md-code-font)";
 	}
 });
-
-// Set Mermaid theme
-mermaid-custom.initialize({
-	securityLevel: 'loose',
-	theme: 'base',
-	themeVariables: {
-		primaryColor: '#BB2528',
-		primaryTextColor: '#fff',
-		primaryBorderColor: '#7C0000',
-		lineColor: '#F8B229',
-		secondaryColor: '#006100',
-		tertiaryColor: '#fff'
-	}
-});

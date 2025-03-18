c543c5f757d952b888a50b69e10a84de54784bfe
GitHub Action <noreply@github.com>
2025-03-18 21:53:31 +0000

    Deployed ca7d225 with MkDocs version: 1.6.1

--- _static/js/snippets.js
+++ _static/js/snippets.js
@@ -9,6 +9,14 @@ admonition.forEach((item) => {
 
 // Set Mermaid theme
 mermaid.initialize({
-  securityLevel: 'loose',
-  theme: 'base',
+	securityLevel: 'loose',
+	theme: 'base',
+	themeVariables: {
+		primaryColor: '#BB2528',
+		primaryTextColor: '#fff',
+		primaryBorderColor: '#7C0000',
+		lineColor: '#F8B229',
+		secondaryColor: '#006100',
+		tertiaryColor: '#fff'
+	}
 });

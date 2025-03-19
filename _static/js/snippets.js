bdc3284ff5ba1bbcfc9316b45d77cb651266702d
GitHub Action <noreply@github.com>
2025-03-19 11:10:11 +0000

    Deployed 7ba5c42 with MkDocs version: 1.6.1

--- _static/js/snippets.js
+++ _static/js/snippets.js
@@ -8,13 +8,13 @@ admonition.forEach((item) => {
 });
 
 // https://mkdocs-mermaid2.readthedocs.io/en/latest/tips/#material-theme-switching-on-the-fly-between-light-and-dark-mode
-var paletteSwitcher1 = document.getElementById("__palette_1");
-var paletteSwitcher2 = document.getElementById("__palette_2");
+var paletteSwitcher1 = document.getElementById("__palette_0");
+var paletteSwitcher2 = document.getElementById("__palette_1");
 
-paletteSwitcher1.addEventListener("change", function () {
+paletteSwitcher0.addEventListener("change", function () {
   location.reload();
 });
 
-paletteSwitcher2.addEventListener("change", function () {
+paletteSwitcher1.addEventListener("change", function () {
   location.reload();
 });

c19a63f583804ba96a96ea951f59caa7b54b78bc
Šebestová <ka.sebestova.cz@gmail.com>
2025-03-19 16:23:20 +0100

    Update snippets.js

--- docs/_static/js/snippets.js
+++ docs/_static/js/snippets.js
@@ -6,22 +6,3 @@ admonition.forEach((item) => {
 		item.style.fontFamily = "var(--md-code-font)";
 	}
 });
-
-// https://mkdocs-mermaid2.readthedocs.io/en/latest/tips/#material-theme-switching-on-the-fly-between-light-and-dark-mode
-var paletteSwitcher0 = document.getElementById("__palette_0");
-var paletteSwitcher1 = document.getElementById("__palette_1");
-
-paletteSwitcher0.addEventListener("change", function () {
-  location.reload();
-});
-
-paletteSwitcher1.addEventListener("change", function () {
-  location.reload();
-});
-
-
-var elems = document.body.getElementsByTagName("math");
-for(var i = 0; i < elems.length; i++)
-{
-  elems[i].setAttribute("mathcolor", "red");
-}
